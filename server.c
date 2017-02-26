bf3bf99a1666527f9c5c1e3bac5247d31bbecc5a
Muzammil Khan <muzammil.a.khan@live.com>
2017-02-26 00:50:08 -0800

    revised to use select and timer

--- server.c
+++ server.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <sys/wait.h>	/* for the waitpid() system call */
 #include <signal.h>	/* signal name macros, and the kill() prototype */
+#include <sys/time.h> // for timer
 
 
 void error(char *msg)
@@ -52,11 +53,16 @@ int main(int argc, char *argv[])
   // restrictions for our TCP implementation
   int max_packet_length = 1024; //includes header, in bytes
   int max_sequence_number = 30720; //bytes    int window_size = 5120; //bytes
-  int rto_val = 500; //retransmission timeout value (ms)
+  int rto_val = 500 * 1000; //retransmission timeout value (ms) but select uses us, so we multiply by 1000
+
+  //timer
+  struct timeval Timer;
 
   int sockfd, portno, pid;
   socklen_t clilen;
   struct sockaddr_in serv_addr, cli_addr;
+  int running = 1; //set to 0 when closing server
+  fd_set active_fd_set; // set for holding sockets
 
   if (argc < 2) {
     fprintf(stderr,"ERROR, no port provided\n");
@@ -80,45 +86,75 @@ int main(int argc, char *argv[])
   char buffer[buffer_size];			 
   memset(buffer, 0, buffer_size);	//reset memory
 
-  //------- Three Way Handshake -------
-  //wait for client to initiate connection
-  clilen = sizeof(cli_addr)
-  bzero(buffer, buffer_size);
-  n = recvfrom(sockfd, buffer, buffer_size, 0, (struct sockaddr *) &cli_addr, &clilen);
-  if(n < 0)
-    error("ERROR in three way handshake: recvfrom");
+  unsigned short source; 
+  unsigned short destination;
+  unsigned int sequence_number;
+  unsigned int acknowledgement_number;
+  unsigned int ACK;
+  unsigned int SYN;
+  unsigned int FIN;
+  unsigned short window_size;
 
-  //TODO: Decode header from client message and set values for response
+  int handshake = 0;
+  int closing = 0;
 
-  //response to client
-  n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &cli_addr, clilen);
-  if (n < 0)  
-    error("ERROR in three way handshake: sendto");
-  //connection established at this point
+  //we are using a hard coded port for our application so source = dest port
+  source = portno;
+  destination = source;
 
-  //wait for client to respond
-  bzero(buffer, buffer_size);
-  n = recvfrom(sockfd, buffer, buffer_size, 0, (struct sockaddr *) &cli_addr, &clilen);
-  if(n < 0)
-    error("ERROR in three way handshake: recvfrom");
-  //TODO: Decode header from client message. 
-  //TODO: Do we retransmit if part of this time out?
 
-  //------- CONNECTION ESTABLISHED -------
+  while(running){
+    
+    FD_ZERO(&active_fd_set);
+    FD_SET(sockfd, &active_fd_set);
+    Timer.tv_sec = 0;
+    Timer.tv_usec = rto_val;
+    n = select(sockfd + 1, &active_fd_set, NULL, NULL, &Timer);
+    if (n < 0) {
+      close(sockfd);
+      //ERROR OCCURED TODO: handle error
+    } else if (n == 0) { //timeout occured
+      //TODO: handle cases where retransmission is necessary here
+    } else { 
+      if (FD_ISSET(sockfd, &active_fd_set)) { // new connection request
+        bzero(buffer, buffer_size);
+        n = recvfrom(sockfd, buffer, buffer_size, 0, (struct sockaddr *) &cli_addr, &clilen);
+        if(n < 0)
+            error("ERROR recvfrom");
+        DecodeTCPHeader(buffer, source, destination, sequence_number, acknowledgement_number, ACK, SYN, FIN, window_size);
+        if(SYN){ // THREE WAY HANDSHAKE
+          ACK = 1;
+          SYN = 1;
+          FIN = 0;
+          acknowledgement_number = sequence_number + 1;
+          sequence_number = 0; 
+          handshake = 1;
+          //respond to client
+          EncodeTCPHeader(buffer, source, destination, sequence_number, acknowledgement_number, ACK, SYN, FIN, window_size);
+          n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &cli_addr, clilen);
+          if (n < 0)  
+            error("ERROR in three way handshake: sendto");
+        }else if (ACK) { //RECIEVED ACK
+          //3 cases: part of 3 way handshake,  ack for closing, or ack for regular data,
+          //TODO: fill in how to handle ACKS
+          if(handshake){
 
-  //keep receiving data from client and respond accordingly
-  /*
-  while(1){
-    bzero(buffer, buffer_size);
-    n = recvfrom(sockfd, buffer, buffer_size, 0, (struct sockaddr *) &cli_addr, &clilen);
-    if(n < 0)
-      error("ERROR in recvfrom");
+          } else if(closing) {
 
-    n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &cli_addr, clilen);
-    if (n < 0)  
-      error("ERROR in sendto");
+          } else {
+
+          }
+
+        }else if (FIN) { //CLOSING CONNECTION
+          //TODO: closing stuff
+        } else { //RECEIVED DATA REQUEST
+
+        }
+
+      }
+    }
+
   }
-  */
 
   close(sockfd);         
   return 0; 
